d493ae694c093112ade7187c50660a90a7f2d087
tlongers <tom@longley.it>
2019-05-21 13:54:56 +0100

    SFM Research Handbook rejig: - tidy up the md to rst convertor script.

--- common_sources/working/sfm_rh_convertor.sh
+++ common_sources/working/sfm_rh_convertor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# Research Handbook: convert from MD to RST for use in Sphinx
+# Research Handbook: convert from Markdown to ReStructuredText for use in Sphinx
 #
 # Tl / 2019-05-20
 
-# Safety
+# Safety measures
 
 set -eu
 shopt -s failglob
@@ -12,12 +12,14 @@ shopt -s failglob
 # Convert from MD to RST
 # Simple loop on in/ folder
 # PANDOC settings: 
-# - no wrapping
-# - assign heading anchors
-# - tell PANDOC input tables are pipe separated
+# - no wrapping, because things get weird otherwise
+# - assign heading anchors by name not specified anchor
+# - tell PANDOC input tables are pipe separated (even though we nuke them later)
 # Use sed to:
+# - process H3 headings to strong emphasis (solves some labelling issues)
 # - convert some internal links to proper internal refs
-# - strip out references to images, which I've yet to figure out
+# - change the path for images
+# - deal with escaped stars and rogue leading/trailing spacing
 
 for i in $(ls in/|sed 's/\.md//g'); do
 
@@ -31,7 +33,6 @@ for i in $(ls in/|sed 's/\.md//g'); do
 	
 	 done
 
-
 # File specific post-processing 
 
 # Remove tables of contents, which aren't converting clearly using PANDOC
@@ -41,5 +42,6 @@ awk '/Summary of fields/ {f=1} /Event: ID/ {f=0} !f;' out/events.rst > tmp && mv
 awk '/Summary of organization fields/ {f=1} /Organization: ID/ {f=0} !f' out/organizations.rst > tmp && mv tmp out/organizations.rst
 awk '/Summary of person fields/ {f=1} /Person: ID/ {f=0} !f' out/persons.rst > tmp && mv tmp out/persons.rst
 
-# Scraps
+# To do
+# - Rationalize and prettify sed statements a bit
 
